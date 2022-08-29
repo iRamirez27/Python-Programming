3da599e12174830faf05156acc6938c6790dfb10
iRamirez27 <112404559+iRamirez27@users.noreply.github.com>
2022-08-29 08:55:51 -0500

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,15 @@
+#Programmer : Izzy Ramirez
+#Program : Arithmetic operations
+#First created : 08/29/2022 8:32:00 CST
+#Last changed : 08/29/2022 8:32:00 CST
+#Version : Python 3.8.2
+
+print("Arithmetic operations");
+firstInput = 5;
+secondInput = 10;
+
+print("sum of given numbers is: ", firstInput+secondInput);
+print("the difference of the given numbers is: ", firstInput-secondInput);
+print("the multiplicaiton of the given numbers is: ", firstInput*secondInput);
+print("the quotient of the given numbers is: ", firstInput/secondInput);
+print("the other quotient of the given numbers is: ", firstInput%secondInput);
