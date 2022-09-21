d954882c6f4b3d76e89222789c54d33fe6eaa06d
Izzy <ipramirez27@yahoo.com>
2022-09-21 08:44:21 -0500

    added alternate approach

--- oddNumber.py
+++ oddNumber.py
@@ -1,10 +1,14 @@
-#Programmer: Izzy Ramirez
-#Program: Odd Number
-#First created: 9/19/22 8:22:00 CST
-#Last changed: 9/21/22 8:07:00 CST
-#Version: Python 3.10.6
+# Programmer: Izzy Ramirez
+# Program: Odd Number
+# First created: 9/19/22 8:22:00 CST
+# Last changed: 9/21/22 8:07:00 CST
+# Version: Python 3.10.6
 
-print("Odd numbers from the first natural hundred numbers")
-for i in range(0, 101):
-    if (i % 2) != 0:
-        print(i, end=" ")
+# print("Odd numbers from the first natural hundred numbers")
+# for i in range(0, 101):
+#     if (i % 2) != 0:
+#         print(i, end=" ")
+
+print("Alternate approach")
+for k in range(1, 101):
+    print(((2*k)-1), end=" ")
