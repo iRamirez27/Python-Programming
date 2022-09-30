17a705bcadf2b3d167ca36f6b13ce5eb9481e80d
Izzy <ipramirez27@yahoo.com>
2022-09-30 09:07:39 -0500

    added input

--- numbersType.py
+++ numbersType.py
@@ -1,44 +1,79 @@
-# Programmer: Izzy Ramirez
-# Program: Numbers Type
-# First Created: 9/26/22 8:03:00 CST
-# Last Edited: 9/26/22 8:46:00 CST
-# Version: 1.0.0
+"""
+Programmer: Izzy Ramirez
+Program: Numbers Type
+First Created: 9/26/22 8:03:00 CST
+Last Edited: 9/30/22 8:50:00 CST
+Version: 1.0.0
+
+"""
+print("Welcome to mathematical operations.")
+print("1: Odd Numbers\n"
+      "2: Even Numbers\n"
+      "3: Prime Numbers\n"
+      "4: Perfect Numbers\n"
+      "5: Palindrome Numbers")
+choice = int(input("\nSelect desired operation by entering a number: "))
 
 # odd numbers
-print("Odd numbers from the first hundred natural numbers")
-for i in range(0, 100):
-    if (i % 2) != 0:
-        print(i, end=" ")
+if choice == 1:
+    def odd_numbers():
+        print("\nOdd numbers from the first hundred natural numbers")
+        for i in range(0, 100):
+            if (i % 2) != 0:
+                print(i, end=" ")
+
+    odd_numbers()
+
 
 # even numbers
-print("\nEven numbers from the first hundred natural numbers")
-for i in range(0, 100):
-    if (i % 2) == 0:
-        print(i, end=" ")
+elif choice == 2:
+    def even_numbers():
+        print("\nEven numbers from the first hundred natural numbers")
+        for i in range(0, 100):
+            if (i % 2) == 0:
+                print(i, end=" ")
+
+    even_numbers()
+
 
 # prime numbers
-print("\nPrime numbers from the first hundred natural numbers")
-counter = 0         # initializing counter variable to 0
-for n in range(1, 101):
-    for i in range(2, int((n / 2) + 1)):  # iterating for numbers between 2 and n/2 + 1
-        # checking number of divisors for a number
-        if n % i == 0:          # check for remainder is 0
-            counter = counter + 1       # increase counter if you find a divisor
-    if counter == 0:            # decision to see if counter is 0
-        print(n, end=" ")       # printing if number is prime
-    counter = 0
+elif choice == 3:
+    def prime_numbers():
+        print("\nPrime numbers from the first hundred natural numbers")
+        counter = 0         # initializing counter variable to 0
+        for n in range(1, 101):
+            for i in range(2, int((n / 2) + 1)):  # iterating for numbers between 2 and n/2 + 1
+                # checking number of divisors for a number
+                if n % i == 0:          # check for remainder is 0
+                    counter = counter + 1       # increase counter if you find a divisor
+            if counter == 0:            # decision to see if counter is 0
+                print(n, end=" ")       # printing if number is prime
+            counter = 0
+
+    prime_numbers()
+
 
 # perfect numbers
-print("\nPerfect numbers from the first hundred natural numbers")
-sum = 0
-check = 0
-for n in range(1, 101):
-    for i in range(1, int((n / 2) + 1)):
-        check = int(n % i)
-        if check == 0:
-            sum = int(sum + i)
-    if n == sum:
-        print(n, end=" ")
-    sum = 0
+elif choice == 4:
+    def perfect_numbers():
+        print("\nPerfect numbers from the first hundred natural numbers")
+        sum = 0
+        check = 0
+        for n in range(1, 101):
+            for i in range(1, int((n / 2) + 1)):
+                check = int(n % i)
+                if check == 0:
+                    sum = int(sum + i)
+            if n == sum:
+                print(n, end=" ")
+            sum = 0
+
+    perfect_numbers()
+
+
 # palindrome numbers
-# print("\nPalindrome numbers from the first hundred natural numbers")
+elif choice == 5:
+    def palindrome_numbers():
+        print("\nPalindrome numbers from the first hundred natural numbers")
+
+    palindrome_numbers()
